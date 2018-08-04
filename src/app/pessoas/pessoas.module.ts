c9a1b7f10f00018f5a12bbb399dac27aa0d393aa
Bruno Batista <brunorafael.o@hotmail.com>
2018-08-04 15:13:15 -0300

    Implementado inclusão de contato em pessoa e seu crud

--- src/app/pessoas/pessoas.module.ts
+++ src/app/pessoas/pessoas.module.ts
@@ -1,19 +1,22 @@
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { InputMaskModule } from 'primeng/inputmask';
 import { TooltipModule } from 'primeng/tooltip';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { DialogModule } from 'primeng/dialog';
 
 import { SharedModule } from './../shared/shared.module';
 import { PessoasRoutingModule } from './pessoas-routing.module';
 
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoaCadastroContatoComponent } from './pessoa-cadastro-contato/pessoa-cadastro-contato.component';
 
 @NgModule({
   imports: [
@@ -23,6 +26,8 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
     InputTextModule,
     ButtonModule,
     TableModule,
+    PanelModule,
+    DialogModule,
     TooltipModule,
     InputMaskModule,
     RouterModule,
@@ -32,7 +37,8 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
   ],
   declarations: [
      PessoaCadastroComponent,
-     PessoasPesquisaComponent
+     PessoasPesquisaComponent,
+     PessoaCadastroContatoComponent
   ],
   exports: [
   ]
